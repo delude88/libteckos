1dbec794cd9c7f5f102f7b50ea8d0dbb17ffcef3
Christof Ruch <christof.ruch@gmx.de>
2022-01-18 13:45:58 +0100

    [fix] Seems we need a fix from nlohmann when we build for an older < 10.15 macOS SDK

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 
 class TeckosConan(ConanFile):
     settings = ["os", "compiler", "build_type", "arch"]
-    requires = ["nlohmann_json/3.10.4", "cpprestsdk/2.10.18"]
+    requires = ["nlohmann_json/3.10.5", "cpprestsdk/2.10.18"]
     generators = "cmake", "cmake_find_package", "json"
 
     def configure(self):
