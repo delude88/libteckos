29f51844742c6eb32ca3697345c96cc32cc47b27
Christof Ruch <christof.ruch@gmx.de>
2022-01-26 18:15:20 +0100

    There is a naming conflict - you have the teckos::Result class twice. Once as a struct, and once as a different typedef.

--- include/teckos/rest.h
+++ include/teckos/rest.h
@@ -9,16 +9,21 @@
 #include <nlohmann/json_fwd.hpp>  // for json
 
 namespace teckos {
-struct Result {
-  int statusCode;
-  std::string statusMessage;
-  nlohmann::json body;
-};
+
+	namespace REST {
+		struct Result {
+			int statusCode;
+			std::string statusMessage;
+			nlohmann::json body;
+		};
+
+	}
+
 typedef std::map<std::string, std::string> Header;
 class rest {
  public:
-  static Result Get(const std::string &url, const Header header = Header());
+  static REST::Result Get(const std::string &url, const Header header = Header());
 
-  static Result Post(const std::string &url, const Header header = Header(), const nlohmann::json &body = nullptr);
+  static REST::Result Post(const std::string &url, const Header header = Header(), const nlohmann::json &body = nullptr);
 };
 }
